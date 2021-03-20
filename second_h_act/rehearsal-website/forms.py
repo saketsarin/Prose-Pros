8f78ab621aeb11663ac0642f3b38975704d88630
kenjidion <mkdion@uci.edu>
2021-03-20 15:53:43 -0700

    Delete second_h_act/rehearsal-website directory

--- second_h_act/rehearsal-website/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class RehearsalForm(forms.Form):
-    sceneNum = forms.IntegerField(label='sceneNum')
-    #character = forms.IntegerField(label='character')
--- second_h_act/rehearsal-website/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from rehearsal_website import views
-
-urlpatterns = [
-    path('', views.mainPage, name='rehearsal_website'),
-    path('play/', views.submit, name='play')
-]
